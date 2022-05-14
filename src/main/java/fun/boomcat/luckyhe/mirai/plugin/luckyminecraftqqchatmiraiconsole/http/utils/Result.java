8cb500c963adc888b8cad83305f923afe142a44c
Lucky_He <673230244@qq.com>
2022-05-14 20:57:40 +0800

    继续封装 Result 和 ResultCode

--- src/main/java/fun/boomcat/luckyhe/mirai/plugin/luckyminecraftqqchatmiraiconsole/http/utils/Result.java
+++ src/main/java/fun/boomcat/luckyhe/mirai/plugin/luckyminecraftqqchatmiraiconsole/http/utils/Result.java
@@ -1,13 +1,65 @@
 package fun.boomcat.luckyhe.mirai.plugin.luckyminecraftqqchatmiraiconsole.http.utils;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Result<T> {
     private final int code;
     private final T data;
     private final String msg;
 
-    public Result(ResultCode resultCode, T data) {
-        this.code = resultCode.getCode();
-        this.msg = resultCode.getMsg();
+    private Result(ResultCode resultCode, T data) {
+        if (resultCode == null) {
+            this.code = -1;
+            this.msg = null;
+        } else {
+            this.code = resultCode.getCode();
+            this.msg = resultCode.getMsg();
+        }
         this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public static <T> String success(T data) {
+        Result<T> result = new Result<>(ResultCode.SUCCESS, data);
+        return toJsonString(result);
+    }
+
+    public static <T> String error(ResultCode resultCode) {
+        Result<T> result = new Result<>(resultCode, null);
+        return toJsonString(result);
+    }
+
+    private static String toJsonString(Result<?> result) {
+        Map<String, Object> jsonMap = new HashMap<>();
+
+        int code = result.getCode();
+        String msg = result.getMsg();
+        Object data = result.getData();
+
+        if (code != -1) {
+            jsonMap.put("code", code);
+        }
+        if (msg != null) {
+            jsonMap.put("msg", msg);
+        }
+        if (data != null) {
+            jsonMap.put("data", data);
+        }
+
+        return JSONObject.toJSONString(jsonMap);
     }
 }
--- src/main/java/fun/boomcat/luckyhe/mirai/plugin/luckyminecraftqqchatmiraiconsole/http/utils/ResultCode.java
+++ src/main/java/fun/boomcat/luckyhe/mirai/plugin/luckyminecraftqqchatmiraiconsole/http/utils/ResultCode.java
@@ -2,9 +2,9 @@ package fun.boomcat.luckyhe.mirai.plugin.luckyminecraftqqchatmiraiconsole.http.u
 
 public enum ResultCode {
     SUCCESS(0, "操作成功"),
-    VERIFY_NEEDED(1, "需要验证"),
-    VERIFY_NOT_PASSED(2, "验证失败"),
-    ERROR(100, "其它错误");
+    SESSION_NOT_EXISTED(1, "会话不存在"),
+    SESSION_EXISTED(2, "会话已存在"),
+    VERIFY_NOT_PASSED(100, "验证密码错误");
 
     private final int code;
     private final String msg;
